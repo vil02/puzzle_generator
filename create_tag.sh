0d371e6b7574bf2f56858d0f0b574b399e4f8c9f
vil02 <65706193+vil02@users.noreply.github.com>
2024-07-27 19:03:58 +0200

    chore: add `create_tag`

---
+++ create_tag.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -euo pipefail
+
+current_branch=$(git branch --show-current)
+readonly current_branch
+if [[ "${current_branch}" != "master" ]]; then
+    echo "You are not on the master."
+    exit 1
+fi
+
+current_version=$(poetry version --short)
+readonly current_version
+if [[ -z "${current_version}" ]]; then
+    echo "Failed to get the current version from poetry."
+    exit 1
+fi
+
+tag_name="v$current_version"
+readonly tag_name
+if [[ "$(git tag -l "${tag_name}")" ]]; then
+    echo "Tag ${tag_name} already exists."
+else
+    git tag "${tag_name}"
+    git push origin tag "${tag_name}"
+fi
