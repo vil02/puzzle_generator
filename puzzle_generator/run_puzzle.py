936484c756897e7b83db0542781190e0dbf600d8
vil02 <65706193+vil02@users.noreply.github.com>
2024-07-29 21:33:57 +0200

    fix: use proper type hints in `run_puzzle`

--- puzzle_generator/run_puzzle.py
+++ puzzle_generator/run_puzzle.py
@@ -6,7 +6,7 @@ from .puzzle_data_encryption import decrypt_data
 
 def run_puzzle(
     in_puzzle: typing.Tuple[str, bytes],
-    in_decrypt: typing.Callable[[bytes, bytes], bytes],
+    in_decrypt: typing.Callable[[bytes, bytes], bytes | None],
     get_answer,
 ):
     question, rest = in_puzzle
