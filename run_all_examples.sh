79b6c9779a229e0a9dde7f30bcfa087bd6aa4b9f
vil02 <65706193+vil02@users.noreply.github.com>
2024-09-09 17:58:15 +0200

    chore: remove `run_all_examples_no_poetry.sh`

--- run_all_examples.sh
+++ run_all_examples.sh
@@ -2,5 +2,15 @@
 
 set -euo pipefail
 
+use_poetry=true
+
+if [[ "${1:-}" == "--no-poetry" ]]; then
+    use_poetry=false
+fi
+
 cd examples/
-find . -name "*.py" -exec poetry run python3 {} +
+if $use_poetry; then
+    find . -name "*.py" -exec poetry run python3 {} +
+else
+    find . -name "*.py" -exec python3 {} +
+fi
--- run_all_examples_no_poetry.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-cd examples/
-find . -name "*.py" -exec python3 {} +
