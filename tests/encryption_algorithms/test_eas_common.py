3223cb2c9890321211338c54f4ef7f65d4f2082d
vil02 <65706193+vil02@users.noreply.github.com>
2025-03-29 19:30:32 +0100

    style: introduce `eas_common`

--- tests/encryption_algorithms/test_eas_common.py
+++ tests/encryption_algorithms/test_eas_common.py
@@ -1,15 +1,15 @@
 import pytest
 
-import puzzle_generator.encryption_algorithms.simple.common as eac
+import puzzle_generator.encryption_algorithms.simple.eas_common as eas_c
 from .. import utils
 
 
 @pytest.mark.parametrize("in_signature_params", utils.SOME_SIGNATURE_PARAMS)
 def test_digest_size(in_signature_params: dict[str, str]) -> None:
-    some_hash = eac.sign_bytes(b"some_msg", b"some_key", in_signature_params)
-    assert eac.digest_size(in_signature_params) == len(some_hash)
+    some_hash = eas_c.sign_bytes(b"some_msg", b"some_key", in_signature_params)
+    assert eas_c.digest_size(in_signature_params) == len(some_hash)
 
 
 def test_split_data_and_signature_raises() -> None:
     with pytest.raises(ValueError, match="in_bytes is shorter than signature_size"):
-        eac.split_data_and_signature(b"0", 2)
+        eas_c.split_data_and_signature(b"0", 2)
