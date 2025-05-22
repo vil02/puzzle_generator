b092b0684dc459ac313012cbcbbd8e3538114428
vil02 <65706193+vil02@users.noreply.github.com>
2025-05-22 16:27:39 +0200

    chore: allow `pytest` params in `generate_coverage_data.sh`

--- generate_coverage_data.sh
+++ generate_coverage_data.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 omitted_paths="tests/*"
 readonly omitted_paths
 
-poetry run coverage run --branch -m pytest
+poetry run coverage run --branch -m pytest "$@"
 poetry run coverage xml --omit="${omitted_paths}"
 poetry run coverage html --omit="${omitted_paths}"
 poetry run coverage report --omit="${omitted_paths}"
