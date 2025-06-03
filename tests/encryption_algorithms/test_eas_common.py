4d3f7f6ce14a7bc75804bbcc25cd18cfa5b22199
vil02 <65706193+vil02@users.noreply.github.com>
2025-06-03 15:28:04 +0200

    style: follow naming convention in `encryption_algorithms`

--- tests/encryption_algorithms/test_eas_common.py
+++ tests/encryption_algorithms/test_eas_common.py
@@ -1,16 +1,16 @@
 import pytest
 
-import puzzle_generator.encryption_algorithms.simple.eas_common as eas_c
+from puzzle_generator.encryption_algorithms.ea_simple import eas_common
 
 from .. import utils
 
 
 @pytest.mark.parametrize("in_signature_params", utils.SOME_SIGNATURE_PARAMS)
 def test_digest_size(in_signature_params: dict[str, str]) -> None:
-    some_hash = eas_c.sign_bytes(b"some_msg", b"some_key", in_signature_params)
-    assert eas_c.digest_size(in_signature_params) == len(some_hash)
+    some_hash = eas_common.sign_bytes(b"some_msg", b"some_key", in_signature_params)
+    assert eas_common.digest_size(in_signature_params) == len(some_hash)
 
 
 def test_split_data_and_signature_raises() -> None:
     with pytest.raises(ValueError, match="in_bytes is shorter than signature_size"):
-        eas_c.split_data_and_signature(b"0", 2)
+        eas_common.split_data_and_signature(b"0", 2)
--- tests/encryption_algorithms/test_eas_spiced.py
+++ tests/encryption_algorithms/test_eas_spiced.py
@@ -1,8 +1,8 @@
 import pytest
 
-import puzzle_generator.encryption_algorithms.simple.spiced as ess
+from puzzle_generator.encryption_algorithms.ea_simple import eas_spiced
 
 
 def test_must_be_nonempty_raises() -> None:
     with pytest.raises(ValueError, match="in_list must be nonempty"):
-        ess.must_be_nonempty([])
+        eas_spiced.must_be_nonempty([])
