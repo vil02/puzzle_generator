ff2ae3c2e70dd5c913e2ab616a6c45d15875ed40
vil02 <65706193+vil02@users.noreply.github.com>
2025-06-26 19:45:57 +0200

    chore: add `pyright`

--- check_all_python_files.sh
+++ check_all_python_files.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 poetry run ruff check .
 poetry run mypy .
+poetry run pyright --warnings .
 poetry run bandit -c bandit.yml -r .
 poetry run isort --profile black --check .
 find . -name "*.py" -exec ./check_python_file.sh {} +
