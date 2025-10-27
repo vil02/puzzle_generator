eac13cb973307caf063540f3245ff4b0232bb7fa
Ojus Mathur <mathurojus17@gmail.com>
2025-10-27 08:33:19 +0530

    Add base32 example file
    This script demonstrates reducing output size using base32 encoding instead of the default base64.

---
+++ examples/base32_to_reduce_output_size.py
@@ -0,0 +1,17 @@
+"""
+This file illustrates how one can reduce the output size
+by using base32 encoding.
+The default encoding is base64.
+"""
+from puzzle_generator.create_puzzle import create
+
+_PUZZLE = [
+    "What is 1+1?",
+    "2",
+    "What is my name?",
+    "Piotr",
+    "Congratulations, you solved this quiz!",
+]
+
+# note that the encoding is set to base32
+print(create(_PUZZLE, encoding="base32"))
